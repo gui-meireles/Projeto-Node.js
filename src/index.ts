af8819afbee15a8b6672ac3ca84d7c430143aaab
guilherme.nascimento@nava.com.br <guilherme.nascimento@nava.com.br>
2022-10-25 13:18:18 -0300

    Insert CRUD on API

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("Executei!");
+import express from "express";
+import statusRoute from "./routes/status.route";
+import usersRoute from "./routes/users.route";
+
+const app = express();
+
+// Configurações da aplicação
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Configurações de rotas
+app.use(usersRoute);
+app.use(statusRoute);
+
+// Inicialização do servidor
+app.listen(4500, () => {
+  console.log("Aplicação executando a porta 4500!");
+});
